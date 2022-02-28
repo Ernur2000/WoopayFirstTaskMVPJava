0a889da61d3091b8522e2c2f77b8a387322aee94
Yernur <ernurbolatov48@gmail.com>
2022-02-28 22:59:46 +0600

    modify Singleton in JobApi

--- app/src/main/java/com/bolatovyernur/woopayfirsttaskmvpjava/model/api/JobAPI.java
+++ app/src/main/java/com/bolatovyernur/woopayfirsttaskmvpjava/model/api/JobAPI.java
@@ -5,15 +5,18 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class JobAPI{
+    private static Retrofit retrofit;
     public static Retrofit getRetrofit(String baseUrl) {
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient okHttpClient =  new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
-        Retrofit retrofit = new Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(baseUrl)
-                .client(okHttpClient)
-                .build();
+        if (retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(baseUrl)
+                    .client(okHttpClient)
+                    .build();
+        }
         return retrofit;
     }
 }
